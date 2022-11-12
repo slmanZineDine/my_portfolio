7db3a85603153bc5e06a70b52efb725a5e435c64
Slman <slman.zinedine1@gmail.com>
2022-11-12 15:04:39 +0300

    fix contact section

---
+++ src/assets/js/script.js
@@ -0,0 +1,21 @@
+// ===================== INPUT FIELD =====================
+// const spans = document.querySelectorAll(".input-title");
+// const inputs = document.querySelectorAll(".input");
+// const inputsField = document.querySelectorAll(".input-field");
+// const inputHandle = (ele) => {
+//    ele.lastElementChild.focus();
+//    spans.forEach((span) => {
+//       if (span.nextElementSibling.value === "") span.classList.remove("active");
+//    });
+//    ele.firstElementChild.classList.add("active");
+// };
+// inputs.forEach((ele) => ele.addEventListener("click", (_) => inputHandle(ele)));
+// inputsField.forEach((ele) =>
+//    ele.addEventListener("focus", (_) => {
+//       spans.forEach((span) => {
+//          if (span.nextElementSibling.value === "")
+//             span.classList.remove("active");
+//       });
+//       ele.previousElementSibling.classList.add("active");
+//    })
+// );
