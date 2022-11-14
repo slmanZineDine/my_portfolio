8e96f45fb3d0a8da9ca1be698dca3329ae8253f6
Slman <slman.zinedine1@gmail.com>
2022-11-14 09:38:35 +0300

    Create scroll-up, make aside opacity, and wirte script to validate form

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -27,19 +27,46 @@ const theName = document.getElementById("name");
 const email = document.getElementById("email");
 const subject = document.getElementById("subject");
 const message = document.getElementById("message");
+const validationMessage = document.getElementById("validation-message");
 const sendEmail = (e) => {
    e.preventDefault();
-   if (theName.value === "" || email.value === "" || subject.value === "")
-      console.log("error");
-   else {
+   if (theName.value === "" || email.value === "" || subject.message === "") {
+      theName.style.borderColor = "red";
+      email.style.borderColor = "red";
+      message.style.borderColor = "red";
+      checkField("Please fill in all required fields.", "red");
+   } else if (
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)
+   ) {
+      email.style.borderColor = "red";
+      checkField("You have entered an invalid email address.", "red");
+   } else {
       // serviceID - templateID - formID - publicKey
-
-      emailjs.sendForm(
-         "service_6ofdhi6",
-         "template_lkosary",
-         "#form",
-         "zswI7LMZy7B8Rd-29"
-      );
+      emailjs
+         .sendForm(
+            "service_6ofdhi6",
+            "template_lkosary",
+            "#form",
+            "zswI7LMZy7B8Rd-29"
+         )
+         .then((_) => {
+            theName.style = "";
+            email.style = "";
+            message.style = "";
+            checkField(
+               "Your e-mail has been successfully sent. Thank you.",
+               "green"
+            );
+         });
+      checkField("Please wait. Processing", "grey");
    }
+};
+const checkField = (message, color) => {
+   validationMessage.textContent = message;
+   validationMessage.style.backgroundColor = color;
+   validationMessage.classList.add("show");
+   setTimeout((_) => {
+      validationMessage.classList.remove("show");
+   }, 4000);
 };
 form.addEventListener("submit", sendEmail);
